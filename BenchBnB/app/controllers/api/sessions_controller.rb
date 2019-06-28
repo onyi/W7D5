150063ce0aae0751b84d1fbc5511370d60e9cbbc
Onyi Cho <onyicho@gmail.com>
2019-06-28 15:13:02 -0700

    Add user and session controller related methods

--- BenchBnB/app/controllers/api/sessions_controller.rb
+++ BenchBnB/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,22 @@
 class Api::SessionsController < ApplicationController
+
+  def destroy
+    if current_user
+      logout
+      render "api/users/show"
+    else
+      render json: ["Already Sign Out"], status: 404
+    end
+  end
+
+  def create
+    @user = User.find_by_credential(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid username/password combination"], status: 401
+    end
+  end
+
 end
